cdc9ae7ad2210e3fb83c411f73c5c30906abe98b
nimitbdave <nimitbdave@gmail.com>
2020-11-01 16:32:50 -0800

    Create 001 Plot Simulation Image.py

---
+++ 001 Plot Simulation Image.py
@@ -0,0 +1,12 @@
+# Import some packages from matplotlib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+# Uncomment the next line for use in a Jupyter notebook
+#%matplotlib inline
+
+# Define the filename, read and plot the image
+filename = 'sample.jpg'
+image = mpimg.imread(filename)
+plt.imshow(image)
+plt.show()
