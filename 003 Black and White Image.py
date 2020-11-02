8b41ce798ce8bb26a0d0bb27f66b25e0a0cd8047
nimitbdave <nimitbdave@gmail.com>
2020-11-01 18:19:08 -0800

    Create 003 Black and White Image.py

---
+++ 003 Black and White Image.py
@@ -0,0 +1,8 @@
+# Define a function to perform a color threshold
+def color_thresh(img, rgb_thresh=(0, 0, 0)):
+    ####### TODO 
+    # Create an empty array the same size in x and y as the image 
+        # but just a single channel
+    # Apply the thresholds for RGB and 
+        # assign 1's where threshold was exceeded
+    return binary_image
