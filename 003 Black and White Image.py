b37ff90b3c1d7e99b96ccc65169af2f60d01ca07
nimitbdave <nimitbdave@gmail.com>
2020-11-01 18:22:07 -0800

    Update 003 Black and White Image.py

--- 003 Black and White Image.py
+++ 003 Black and White Image.py
@@ -1,3 +1,24 @@
+#STEP 1:  Get the image.  
+
+# Import some packages from matplotlib
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+	
+# Uncomment the next line for use in a Jupyter notebook
+#%matplotlib inline
+	
+# Define the filename, read and plot the image
+filename = 'sample.jpg'
+image = mpimg.imread(filename)
+plt.imshow(image)
+plt.show()
+
+
+
+
+
+
+
 # Define a function to perform a color threshold
 def color_thresh(img, rgb_thresh=(0, 0, 0)):
     ####### TODO 
